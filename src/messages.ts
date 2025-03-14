0f68b7f9b8cdca4bff0b85c8fb1c89ee4a6b148d
Micha Buchholz <micha@haus23.net>
2025-03-14 15:06:31 +0100

    feature: Add a create message mutation.

--- src/messages.ts
+++ src/messages.ts
@@ -1,7 +1,18 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const recent = query({
   handler: async (ctx) => {
     return await ctx.db.query("messages").take(10);
+  },
+});
+
+export const create = mutation({
+  args: {
+    user: v.string(),
+    body: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("messages", args);
   },
 });
