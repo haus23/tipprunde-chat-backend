2d967b34dc096e2b3da2bed7517cc313fbcd7b05
Micha Buchholz <micha@haus23.net>
2025-03-14 20:15:52 +0100

    feat: Authenticate functions via service key.

--- src/messages.ts
+++ src/messages.ts
@@ -1,10 +1,13 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { paginationOptsValidator } from "convex/server";
 
 export const list = query({
-  args: { paginationOpts: paginationOptsValidator },
-  handler: async (ctx, { paginationOpts }) => {
+  args: { paginationOpts: paginationOptsValidator, apiKey: v.string() },
+  handler: async (ctx, { paginationOpts, apiKey }) => {
+    if (apiKey !== process.env.API_KEY) {
+      throw new ConvexError("Unauthorized");
+    }
     return await ctx.db
       .query("messages")
       .order("desc")
@@ -16,8 +19,12 @@ export const create = mutation({
   args: {
     user: v.string(),
     body: v.string(),
+    apiKey: v.string(),
   },
-  handler: async (ctx, args) => {
-    await ctx.db.insert("messages", args);
+  handler: async (ctx, { apiKey, ...data }) => {
+    if (apiKey !== process.env.API_KEY) {
+      throw new ConvexError("Unauthorized");
+    }
+    await ctx.db.insert("messages", data);
   },
 });
