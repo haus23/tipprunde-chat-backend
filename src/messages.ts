16987502fc61f9e84b7da27a897f5a56c222aa75
Micha Buchholz <micha@haus23.net>
2025-03-14 15:30:04 +0100

    refactor: Order recent messages ascending...

--- src/messages.ts
+++ src/messages.ts
@@ -3,7 +3,7 @@ import { mutation, query } from "./_generated/server";
 
 export const recent = query({
   handler: async (ctx) => {
-    return await ctx.db.query("messages").take(10);
+    return await ctx.db.query("messages").order("desc").take(10);
   },
 });
 
