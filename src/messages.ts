ff2971a1303abbb264d31995565307b6313c962e
Micha Buchholz <micha@haus23.net>
2025-03-14 16:40:21 +0100

    feat: Add paginated list query.

--- src/messages.ts
+++ src/messages.ts
@@ -1,9 +1,20 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { paginationOptsValidator } from "convex/server";
 
 export const recent = query({
   handler: async (ctx) => {
     return await ctx.db.query("messages").order("desc").take(10);
+  },
+});
+
+export const list = query({
+  args: { paginationOpts: paginationOptsValidator },
+  handler: async (ctx, { paginationOpts }) => {
+    return await ctx.db
+      .query("messages")
+      .order("desc")
+      .paginate(paginationOpts);
   },
 });
 
