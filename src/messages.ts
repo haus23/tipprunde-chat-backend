0c4758723031f9a7598782d2b27dabd6f7f62c2b
Micha Buchholz <micha@haus23.net>
2025-03-14 16:47:52 +0100

    feat: Remove unused recent query.

--- src/messages.ts
+++ src/messages.ts
@@ -2,12 +2,6 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { paginationOptsValidator } from "convex/server";
 
-export const recent = query({
-  handler: async (ctx) => {
-    return await ctx.db.query("messages").order("desc").take(10);
-  },
-});
-
 export const list = query({
   args: { paginationOpts: paginationOptsValidator },
   handler: async (ctx, { paginationOpts }) => {
